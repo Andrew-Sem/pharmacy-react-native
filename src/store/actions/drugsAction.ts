a946028be9c767cbd9e4cdd94bd9f80d45e5238f
Andrew-Sem <avvyayvayva@gmail.com>
2022-11-07 00:16:47 +0300

    added typePrefix to fetchDrugs action

--- src/store/actions/drugsAction.ts
+++ src/store/actions/drugsAction.ts
@@ -2,7 +2,7 @@ import {createAsyncThunk} from "@reduxjs/toolkit";
 import {DrugsService} from "../../services/DrugsService";
 
 export const fetchDrugs = createAsyncThunk(
-    '',
+    'drugs/fetchCatalogue',
     async () => {
         const response = await DrugsService.fetchDrugs()
         return response.data
